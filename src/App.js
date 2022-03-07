8886b3e1476bb619d42c2303f63fec9443bcd0ae
Hafizur Rahman Fahim <hafijurrahman046@gmail.com>
2022-03-08 01:17:39 +0600

    configuring socket.io using context api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import io from "socket.io-client";
 
 //importing pages/routes
 import LandingPage from "./routes/landingPage";
@@ -11,13 +12,14 @@ import SocketContext from "./socket-middleware/socket-context";
 
 //importing services
 import { useGetSelfQuery } from "./services/user";
+import { useEffect, useState } from "react";
 
 function RedirectUnAthenticated(props) {
   const { isLoading, error } = useGetSelfQuery();
   if (isLoading) return <div>Loading...</div>;
   let isAuthenticated = error?.status !== 401;
   if (!isAuthenticated) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/" />;
   }
   return <props.Component {...props} />;
 }
@@ -25,28 +27,37 @@ function RedirectUnAthenticated(props) {
 function App() {
   const { isLoading, error } = useGetSelfQuery();
   let isAuthenticated = error?.status !== 401;
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    if (!isAuthenticated) return;
+    const socket = io();
+    setSocket(socket);
+  }, [isLoading]);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            !isAuthenticated ? <LandingPage /> : <Navigate to="/dashboard" />
-          }
-        />
+    <SocketContext.Provider value={socket}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              !isAuthenticated ? <LandingPage /> : <Navigate to="/dashboard" />
+            }
+          />
 
-        <Route
-          path="/dashboard"
-          element={<RedirectUnAthenticated Component={Dashboard} />}
-        />
-        <Route path="/api/github/callback" element={<GithubCallback />} />
-      </Routes>
-    </BrowserRouter>
+          <Route
+            path="/dashboard"
+            element={<RedirectUnAthenticated Component={Dashboard} />}
+          />
+          <Route path="/api/github/callback" element={<GithubCallback />} />
+        </Routes>
+      </BrowserRouter>
+    </SocketContext.Provider>
   );
 }
 
--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 //import { useDispatch } from "react-redux";
 import { useGetSelfQuery } from "../services/user";
+import SocketContext from "../socket-middleware/socket-context";
+import TestDeeplyNestedComponent from "./test-deeply-nested-component";
 
 function Loader() {
   return <h1>Loading....</h1>;
@@ -13,5 +15,12 @@ export default function Dashboard() {
   }, []);
 
   if (isLoading) return <Loader />;
-  return <h1>Hello, {data?.username}</h1>;
+  return (
+    <>
+      <h1>Hello, {data?.username}</h1>
+      <SocketContext.Consumer>
+        {(socket) => <TestDeeplyNestedComponent socket={socket} />}
+      </SocketContext.Consumer>
+    </>
+  );
 }
